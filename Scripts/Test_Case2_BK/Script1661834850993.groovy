e83719d8f1065f3bcc223e0e1421c397a3a5d487
Marfiansyah <Marfiansyah@192.168.4.214>
2022-08-30 11:59:34 +0700

    Tes setelah jalan

--- Scripts/Test_Case2_BK/Script1661834850993.groovy
+++ Scripts/Test_Case2_BK/Script1661834850993.groovy
@@ -17,3 +17,60 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://www.bukalapak.com/')
+
+WebUI.click(findTestObject('Object Repository/Page_Situs Belanja Online dan Jual Beli Mud_3c8940/p_Login'))
+
+WebUI.click(findTestObject('Object Repository/Page_Login ke Akun  Bukalapak/div_Nomor handphone atau email'))
+
+WebUI.setText(findTestObject('Object Repository/Page_Login ke Akun  Bukalapak/input_Nomor handphone atau email_LoginID'), 
+    'fianforza86@gmail.com')
+
+WebUI.click(findTestObject('Object Repository/Page_Login ke Akun  Bukalapak/button_Lanjut'))
+
+WebUI.click(findTestObject('Object Repository/Page_Login ke Akun  Bukalapak/div_Pilih cara lain'))
+
+WebUI.click(findTestObject('Object Repository/Page_Login ke Akun  Bukalapak/svg_Lupa Password_ml-16 svg-icon svg-fill'))
+
+WebUI.click(findTestObject('Object Repository/Page_Login ke Akun  Bukalapak/button_Lanjut'))
+
+WebUI.click(findTestObject('Object Repository/Page_Login ke Akun  Bukalapak/svg_--8753_ico_chevron_down_minor'))
+
+WebUI.click(findTestObject('Object Repository/Page_Login ke Akun  Bukalapak/svg_--8753_ico_chevron_up_minor'))
+
+WebUI.click(findTestObject('Object Repository/Page_Login ke Akun  Bukalapak/strong_--8753'))
+
+WebUI.click(findTestObject('Object Repository/Page_Login ke Akun  Bukalapak/path'))
+
+WebUI.setText(findTestObject('Object Repository/Page_Login ke Akun  Bukalapak/input_Nomor handphone atau email_LoginID'), 
+    'fianforza86@gmail.com')
+
+WebUI.sendKeys(findTestObject('Object Repository/Page_Login ke Akun  Bukalapak/input_Nomor handphone atau email_LoginID'), 
+    Keys.chord(Keys.ENTER))
+
+WebUI.click(findTestObject('Object Repository/Page_Login ke Akun  Bukalapak/div_Pilih cara lain'))
+
+WebUI.click(findTestObject('Object Repository/Page_Login ke Akun  Bukalapak/svg_--8753_ico_chevron_right_minor'))
+
+WebUI.click(findTestObject('Object Repository/Page_Login ke Akun  Bukalapak/svg_Lupa Password_ml-16 svg-icon svg-fill'))
+
+WebUI.doubleClick(findTestObject('Object Repository/Page_Login ke Akun  Bukalapak/input_Nomor handphone atau email_LoginID'))
+
+WebUI.setText(findTestObject('Object Repository/Page_Login ke Akun  Bukalapak/input_Nomor handphone atau email_LoginID'), 
+    '081219183568')
+
+WebUI.click(findTestObject('Object Repository/Page_Login ke Akun  Bukalapak/button_Lanjut'))
+
+WebUI.click(findTestObject('Object Repository/Page_Login ke Akun  Bukalapak/strong_0812-1918-3568'))
+
+WebUI.setText(findTestObject('Object Repository/Page_Login ke Akun  Bukalapak/input_Nomor hilang atau tidak aktif_bl-text_eb5b69'), 
+    '62185')
+
+WebUI.click(findTestObject('Object Repository/Page_Login ke Akun  Bukalapak/span_Verifikasi'))
+
+WebUI.click(findTestObject('Object Repository/Page_Situs Belanja Online dan Jual Beli Mud_3c8940/div_Lagi Populer_bl-image-group__head'))
+
+WebUI.closeBrowser()
+
